89dd60e0a0ae2e6aa471a0d3857d13b43b74d56d
nkalpak <nikita.kalpakovski@outlook.com>
2022-03-22 14:09:03 +0100

    chore(server): add database connection

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,48 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { z } from 'zod';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: getEnvironmentFileName(process.env.NODE_ENV),
+      validate: (config) => {
+        const schema = z.object({
+          DATABASE_HOST: z.string(),
+          DATABASE_USER: z.string(),
+          DATABASE_NAME: z.string(),
+          DATABASE_PORT: z.string().transform(Number),
+          DATABASE_PASSWORD: z.string(),
+        });
+
+        return schema.parse(config);
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: Number.parseInt(process.env.DATABASE_PORT!),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV !== 'production',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+function getEnvironmentFileName(environment: string | undefined) {
+  if (environment == undefined) {
+    return '.env';
+  }
+
+  return ['.', environment, '.env'].join('');
+}
